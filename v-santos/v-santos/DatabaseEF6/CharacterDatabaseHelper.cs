5ba4b1bd65a5003c82c9885936a74d0be2829231
Dawid K <0demongamer0@users.noreply.github.com>
2017-05-13 04:54:37 +0000

    Konwesja klasy MySqlDatabaseHelper do mniejszych klas w celu zastosowania z EF6.

---
+++ v-santos/v-santos/DatabaseEF6/CharacterDatabaseHelper.cs
@@ -0,0 +1,39 @@
+﻿using Serverside.DatabaseEF6.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Serverside.DatabaseEF6
+{
+    public class CharacterDatabaseHelper
+    {
+        public List<Character> SelectCharactersList(long accountId)
+        {
+            return RoleplayConnection.Instance.Characters.Where(x => x.AID == accountId).ToList();
+        }
+
+        public Character SelectCharacter(long cid)
+        {
+            return RoleplayConnection.Instance.Characters.Where(x => x.CID == cid).FirstOrDefault();
+        }
+
+        public void AddCharacter(Character buliding)
+        {
+            RoleplayConnection.Instance.Characters.Add(buliding);
+            RoleplayConnection.Instance.SaveChanges();
+        }
+
+        public void UpdateCharacter(Character buliding)
+        {
+            RoleplayConnection.Instance.Characters.Attach(buliding);
+            RoleplayConnection.Instance.Entry(buliding).State = System.Data.Entity.EntityState.Modified;
+            RoleplayConnection.Instance.SaveChanges();
+        }
+
+        public void DeleteCharacter(long cid)
+        {
+            Character delobj = RoleplayConnection.Instance.Characters.Where(x => x.CID == cid).FirstOrDefault();
+            RoleplayConnection.Instance.Characters.Remove(delobj);
+            RoleplayConnection.Instance.SaveChanges();
+        }
+    }
+}
---
+++ v-santos/v-santos/DatabaseEF6/WorkerDatabaseHelper.cs
@@ -0,0 +1,39 @@
+﻿using Serverside.DatabaseEF6.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Serverside.DatabaseEF6
+{
+    public class WorkerDatabaseHelper
+    {
+        public List<Worker> SelectWorkersList(long groupUid)
+        {
+            return RoleplayConnection.Instance.Workers.Where(x => x.GID == groupUid).ToList();
+        }
+
+        public Worker SelectWorker(long workerId)
+        {
+            return RoleplayConnection.Instance.Workers.Where(x => x.WID == workerId).FirstOrDefault();
+        }
+
+        public void AddWorker(Worker worker)
+        {
+            RoleplayConnection.Instance.Workers.Add(worker);
+            RoleplayConnection.Instance.SaveChanges();
+        }
+
+        public void UpdateWorker(Worker worker)
+        {
+            RoleplayConnection.Instance.Workers.Attach(worker);
+            RoleplayConnection.Instance.Entry(worker).State = System.Data.Entity.EntityState.Modified;
+            RoleplayConnection.Instance.SaveChanges();
+        }
+
+        public void DeleteWorker(long wid)
+        {
+            Worker delobj = RoleplayConnection.Instance.Workers.Where(x => x.WID == wid).FirstOrDefault();
+            RoleplayConnection.Instance.Workers.Remove(delobj);
+            RoleplayConnection.Instance.SaveChanges();
+        }
+    }
+}
