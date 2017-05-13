0c775bfc5af30357815e31a1cb0154881de26fe9
Dawid K <pyra868@gmail.com>
2017-05-13 04:52:51 +0000

    Uzupełnienie brakujących modeli.

--- v-santos/v-santos/DatabaseEF6/Models/Description.cs
+++ v-santos/v-santos/DatabaseEF6/Models/Description.cs
@@ -1,6 +1,13 @@
-﻿namespace Serverside.DatabaseEF6.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Serverside.DatabaseEF6.Models
 {
     public class Description
     {
+        [Key]
+        public long DID { get; set; }
+        public long CID { get; set; }
+        public string Title { get; set; }
+        public string Descriptionn { get; set; }
     }
 }
--- v-santos/v-santos/DatabaseEF6/Models/TelephoneContact.cs
+++ v-santos/v-santos/DatabaseEF6/Models/TelephoneContact.cs
@@ -1,6 +1,15 @@
-﻿namespace Serverside.DatabaseEF6.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Serverside.DatabaseEF6.Models
 {
     public class TelephoneContact
     {
+        [Key]
+        //ID kontaktu
+        public long COID { get; set; }
+        //Id telefonu czyli id przedmiotu telefonu
+        public long TID { get; set; }
+        public string Name { get; set; }
+        public int Number { get; set; }
     }
 }
