1bce29a6af61f6fb35309a694b357acaff653fcf
Dawid K <0demongamer0@users.noreply.github.com>
2017-05-13 04:02:08 +0000

    Przygotowanie do przejścia na Entity Framework 6, konwersja klas na modele oraz dodanie klasy dbcontext.

---
+++ v-santos/v-santos/DatabaseEF6/Models/Character.cs
@@ -0,0 +1,68 @@
+﻿using GTANetworkShared;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Serverside.DatabaseEF6.Models
+{
+    public class Character
+    {
+        [Key]
+        public long CID { get; set; }
+        public long AID { get; set; }
+
+        public DateTime? LastLoginTime { get; set; }
+        public DateTime? TodayPlayedTime { get; set; }
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public decimal Money { get; set; }
+        public long BankAccountNumber { get; set; }
+        public decimal BankMoney { get; set; }
+
+        //Tak wiem, że to niepoprawne politycznie
+        public bool Gender { get; set; }
+
+        public short Weight { get; set; }
+        public DateTime BornDate { get; set; }
+        public short Height { get; set; }
+
+        public short Force { get; set; }
+        public short RunningEfficiency { get; set; }
+        public short DivingEfficiency { get; set; }
+
+        public long? FirstGID { get; set; }
+        public long? SecondGID { get; set; }
+        public long? ThirdGID { get; set; }
+
+        public bool HasIDCard { get; set; }
+        public bool HasDrivingLicense { get; set; }
+
+        //Pomysł zezwolenie na bron jako enum, zeby byly różne typy licencji
+
+        public string ForumDescription { get; set; }
+        public string Story { get; set; }
+
+        public bool IsAlive { get; set; }
+
+        public int HitPoits { get; set; }
+
+        public Vector3 LastPosition { get; set; }
+        public Vector3 SpawnPosition { get; set; }
+
+        public int CurrentDimension { get; set; }
+
+        public int BWState { get; set; }
+
+        //TODO Dodać do helpera
+        public bool? IsCreated { get; set; }
+        //Pole determinuje czy gracz będzie grał w gotowym skinie, czy może sam sobie zrobi
+        public bool Freemode { get; set; }
+        public int? Skin { get; set; }
+
+        //TODO Dodać wszystkie pola na możliwości ubrania
+        public int? Job { get; set; }
+        public decimal? MoneyJob { get; set; }
+        public decimal? JobLimit { get; set; }
+    }
+}
---
+++ v-santos/v-santos/DatabaseEF6/RoleplayConnection.cs
@@ -0,0 +1,39 @@
+﻿using Serverside.DatabaseEF6.Models;
+using System.Data.Entity;
+
+namespace Serverside.DatabaseEF6
+{
+    public class RoleplayConnection : DbContext
+    {
+        public RoleplayConnection() : base("name=RoleplayConnection")
+        {
+            Configuration.LazyLoadingEnabled = false;
+            //Database.SetInitializer(new CreateDatabaseIfNotExists<DbContext>());
+            Database.CreateIfNotExists();
+            Database.Initialize(true);
+        }
+
+        private static RoleplayConnection _instance;
+
+        public static RoleplayConnection Instance
+        {
+            get
+            {
+                if (_instance != null) return _instance;
+                return _instance = new RoleplayConnection();
+            }
+            private set { }
+        }
+
+        public virtual DbSet<Character> Characters { get; set; }
+        public virtual DbSet<Vehicle> Vehicles { get; set; }
+        public virtual DbSet<Group> Groups { get; set; }
+        public virtual DbSet<Item> Items { get; set; }
+        public virtual DbSet<Building> Buildings { get; set; }
+        public virtual DbSet<CrimeBot> CrimeBots { get; set; }
+        public virtual DbSet<Description> Descriptions { get; set; }
+        public virtual DbSet<TelephoneContact> TelephoneContacts { get; set; }
+        public virtual DbSet<TelephoneMessage> TelephoneMessages { get; set; }
+        public virtual DbSet<Worker> Workers { get; set; }
+    }
+}
